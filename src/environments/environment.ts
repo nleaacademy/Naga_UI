34e548245f4a3bf5a0ca1752a79363c42a22bce6
jestin <nleaacademy@gmail.com>
2023-05-28 11:16:58 +0530

    change url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl:'http://localhost:8081/nagas/api'
+ // apiUrl:'http://localhost:8081/nagas/api'
+  apiUrl:'https://nlea-api.azurewebsites.net/nagas/api'
 };
 
 /*
